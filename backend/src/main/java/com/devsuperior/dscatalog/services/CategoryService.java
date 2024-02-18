5b0c1ea781e537524ca48a420143245bc0abd868
IgorTecnologia <iguigu2019@gmail.com>
2024-02-18 20:12:39 -0300

    Category Service

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -34,5 +34,14 @@ public class CategoryService {
 		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
 		return new CategoryDTO(entity);
 	}
+
+	@Transactional
+	public CategoryDTO post(CategoryDTO dto) {
+		Category entity = new Category();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new CategoryDTO(entity);
+		
+	}
 	
 }
