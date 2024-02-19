673134b7fadd1e0d8afa4e3190f717d939fd14e2
IgorTecnologia <iguigu2019@gmail.com>
2024-02-19 15:51:45 -0300

    Import.sql

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO TB_CATEGORY(name) VALUES ('Books');
-INSERT INTO TB_CATEGORY(name) VALUES ('Electronics');
-INSERT INTO TB_CATEGORY(NAME) VALUES ('Computers');
+INSERT INTO TB_CATEGORY(name, created_At) VALUES ('Books', NOW());
+INSERT INTO TB_CATEGORY(name, created_At) VALUES ('Electronics', NOW());
+INSERT INTO TB_CATEGORY(NAME, created_At) VALUES ('Computers', NOW());
