bab071e827007c6607e1230436ce8b9fdf09204c
IgorTecnologia <iguigu2019@gmail.com>
2024-04-10 10:33:50 -0300

    Category Resource

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -30,17 +31,9 @@ public class CategoryResource {
 	private CategoryService service;
 		
 	@GetMapping
-	public ResponseEntity<Page<CategoryDTO>> findAll(
-			@RequestParam(value = "page", defaultValue = "0") Integer page,
-			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
-			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
-			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy
-			
-			) {
+	public ResponseEntity<Page<CategoryDTO>> findAll(Pageable pageable) {
 		
-		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
-		
-		Page<CategoryDTO> list = service.findAllPaged(pageRequest);
+		Page<CategoryDTO> list = service.findAllPaged(pageable);
 		
 		return ResponseEntity.ok().body(list);
 	}
