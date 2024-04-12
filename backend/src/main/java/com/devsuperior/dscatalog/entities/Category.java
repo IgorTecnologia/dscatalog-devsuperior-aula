21cd483d20846b0f5d5b0e8d30fc143864c878d4
IgorTecnologia <iguigu2019@gmail.com>
2024-04-12 15:31:22 -0300

    Category Entity

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -2,13 +2,16 @@ package com.devsuperior.dscatalog.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
@@ -29,8 +32,11 @@ public class Category implements Serializable {
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant updatedAt;
 	
+	@ManyToMany(mappedBy = "categories")
+	private Set<Product> Products = new HashSet<>() ;
+	
 	public Category() {
-}
+	}
 
 	public Category(Long id, String name) {
 		this.id = id;
@@ -70,6 +76,10 @@ public class Category implements Serializable {
 		updatedAt = Instant.now();
 	}
 	
+	public Set<Product> getProducts() {
+		return Products;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
