29d2e8a0efec2f0d048c8d15c236959349f85376
IgorTecnologia <iguigu2019@gmail.com>
2024-04-18 18:32:03 -0300

    User Repository

--- backend/src/main/java/com/devsuperior/dscatalog/repositories/UserRepository.java
+++ backend/src/main/java/com/devsuperior/dscatalog/repositories/UserRepository.java
@@ -3,7 +3,6 @@ package com.devsuperior.dscatalog.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.devsuperior.dscatalog.entities.Product;
 import com.devsuperior.dscatalog.entities.User;
 
 @Repository
